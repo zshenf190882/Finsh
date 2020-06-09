49db147d8b8952d17f1595ba4586c3ca69874828
zshenf190882 <1908827735@qq.com>
2020-06-09 23:23:06 +0800

    登录布局

---
+++ app/src/main/java/com/example/date/LoginActivity.java
@@ -0,0 +1,128 @@
+package com.example.date;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button btn_login;//登录按钮
+    private String uerName,password,spPsw;//获取的用户名，密码，加密密码
+    private EditText user_input,password_input;//编辑框
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+//设置此界面为竖屏
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+        init();
+    }
+
+    private void init() {
+
+        btn_login=findViewById(R.id.btn_login);
+        user_input=findViewById(R.id.user_input);
+        password_input=findViewById(R.id.password_input);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //开始登录，获取用户名和密码 getText().toString().trim();
+                uerName=user_input.getText().toString().trim();
+                password=password_input.getText().toString().trim();
+                //对当前用户输入的密码进行MD5加密再进行比对判断, MD5Utils.md5( ); psw 进行加密判断是否一致
+                //String md5Psw= MD5Utils.md5(password);
+                // md5Psw ; spPsw 为 根据从SharedPreferences中用户名读取密码
+                // 定义方法 readPsw为了读取用户名，得到密码
+                spPsw=readPsw(uerName);
+                // TextUtils.isEmpty
+                if(TextUtils.isEmpty(uerName)){
+                    Toast.makeText(LoginActivity.this, "请输入用户名", Toast.LENGTH_SHORT).show();
+                    return;
+                }else if(TextUtils.isEmpty(password)){
+                    Toast.makeText(LoginActivity.this, "请输入密码", Toast.LENGTH_SHORT).show();
+                    return;}
+                    // md5Psw.equals(); 判断，输入的密码加密后，是否与保存在SharedPreferences中一致
+//                }else if(md5Psw.equals(spPsw)){
+//                    //一致登录成功
+//                    Toast.makeText(LoginActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
+//                    //保存登录状态，在界面保存登录的用户名 定义个方法 saveLoginStatus boolean 状态 , userName 用户名;
+//                    saveLoginStatus(true, uerName);
+//                    //登录成功后关闭此页面进入主页
+//                    Intent data=new Intent();
+//                    //datad.putExtra( ); name , value ;
+//                    data.putExtra("isLogin",true);
+//                    //RESULT_OK为Activity系统常量，状态码为-1
+//                    // 表示此页面下的内容操作成功将data返回到上一页面，如果是用back返回过去的则不存在用setResult传递data值
+//                    setResult(RESULT_OK,data);
+//                    //销毁登录界面
+//                    LoginActivity.this.finish();
+//                    //跳转到主界面，登录成功的状态传递到 MainActivity 中
+//                    startActivity(new Intent(LoginActivity.this, UserActivity.class));
+//                    return;
+//                }else if((spPsw!=null&&!TextUtils.isEmpty(spPsw)&&!md5Psw.equals(spPsw))){
+//                    Toast.makeText(LoginActivity.this, "输入的用户名和密码不一致", Toast.LENGTH_SHORT).show();
+//                    return;
+//                }
+                else{
+                    Toast.makeText(LoginActivity.this, "此用户名不存在", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    /**
+     *从SharedPreferences中根据用户名读取密码
+     */
+    private String readPsw(String userName){
+        //getSharedPreferences("loginInfo",MODE_PRIVATE);
+        //"loginInfo",mode_private; MODE_PRIVATE表示可以继续写入
+        SharedPreferences sp=getSharedPreferences("loginInfo", MODE_PRIVATE);
+        //sp.getString() userName, "";
+        return sp.getString(userName , "");
+    }
+    /**
+     *保存登录状态和登录用户名到SharedPreferences中
+     */
+    private void saveLoginStatus(boolean status,String userName){
+        //saveLoginStatus(true, userName);
+        //loginInfo表示文件名  SharedPreferences sp=getSharedPreferences("loginInfo", MODE_PRIVATE);
+        SharedPreferences sp=getSharedPreferences("loginInfo", MODE_PRIVATE);
+        //获取编辑器
+        SharedPreferences.Editor editor=sp.edit();
+        //存入boolean类型的登录状态
+        editor.putBoolean("isLogin", status);
+        //存入登录状态时的用户名
+        editor.putString("loginUserName", userName);
+        //提交修改
+        editor.commit();
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+        if(data!=null){
+            //是获取注册界面回传过来的用户名
+            // getExtra().getString("***");
+            String userName=data.getStringExtra("userName");
+            if(!TextUtils.isEmpty(userName)){
+                //设置用户名到 et_user_name 控件
+                user_input.setText(userName);
+                //et_user_name控件的setSelection()方法来设置光标位置
+                user_input.setSelection(userName.length());
+            }
+        }
+    }
+}
+   // @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_login);
+//    }
+
--- app/src/main/java/com/example/date/MainActivity.java
+++ app/src/main/java/com/example/date/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.date;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn =(Button)findViewById(R.id.btn_login);
+        Button btn2 =(Button)findViewById(R.id.btn_register);
+
+    }public void btnClick(View btn){
+        openConfig();
+
+    }
+
+    private void openConfig() {
+        Intent config = new Intent(this, LoginActivity.class);
+        startActivity(config);
     }
 }
