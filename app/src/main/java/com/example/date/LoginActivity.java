14b4470d6eece2ec2e211f640b5c52c31df103b1
zshenf190882 <1908827735@qq.com>
2020-06-11 18:59:02 +0800

    注册和登录

--- app/src/main/java/com/example/date/LoginActivity.java
+++ app/src/main/java/com/example/date/LoginActivity.java
@@ -7,6 +7,7 @@ import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,8 +15,9 @@ import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     private Button btn_login;//登录按钮
-    private String uerName,password,spPsw;//获取的用户名，密码，加密密码
-    private EditText user_input,password_input;//编辑框
+    private final String TAG = "Rate";
+    private String uerName,password,spPsw,uer,pas;//获取的用户名，密码，加密密码
+    EditText user_input,password_input;//编辑框
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,37 +39,22 @@ public class LoginActivity extends AppCompatActivity {
                 //开始登录，获取用户名和密码 getText().toString().trim();
                 uerName=user_input.getText().toString().trim();
                 password=password_input.getText().toString().trim();
-                //对当前用户输入的密码进行MD5加密再进行比对判断, MD5Utils.md5( ); psw 进行加密判断是否一致
-                String md5Psw= password;
-                // md5Psw ; spPsw 为 根据从SharedPreferences中用户名读取密码
-                // 定义方法 readPsw为了读取用户名，得到密码
-                spPsw=readPsw(uerName);
-                // TextUtils.isEmpty
-                if(TextUtils.isEmpty(uerName)){
+                SharedPreferences sp=getSharedPreferences("loginInfo", MODE_PRIVATE);
+                uer=sp.getString("userName","");
+                pas=sp.getString("psw","");
+                Log.i(TAG,pas);
+                Log.i(TAG,uer);
+                if(uerName.length()<=0){
                     Toast.makeText(LoginActivity.this, "请输入用户名", Toast.LENGTH_SHORT).show();
                     return;
-                }else if(TextUtils.isEmpty(password)){
+                }else if(password.length()<=0){
                     Toast.makeText(LoginActivity.this, "请输入密码", Toast.LENGTH_SHORT).show();
                     return;
                     // md5Psw.equals(); 判断，输入的密码加密后，是否与保存在SharedPreferences中一致
-                }else if(md5Psw.equals(spPsw)){
-                    //一致登录成功
-                    Toast.makeText(LoginActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
-                    //保存登录状态，在界面保存登录的用户名 定义个方法 saveLoginStatus boolean 状态 , userName 用户名;
-                    saveLoginStatus(true, uerName);
-                    //登录成功后关闭此页面进入主页
-                    Intent data=new Intent();
-                    //datad.putExtra( ); name , value ;
-                    data.putExtra("isLogin",true);
-                    //RESULT_OK为Activity系统常量，状态码为-1
-                    // 表示此页面下的内容操作成功将data返回到上一页面，如果是用back返回过去的则不存在用setResult传递data值
-                    setResult(RESULT_OK,data);
-                    //销毁登录界面
-                    LoginActivity.this.finish();
-                    //跳转到主界面，登录成功的状态传递到 MainActivity 中
+                }else if(password.equals(pas)&& uerName.equals(uer)){
                     startActivity(new Intent(LoginActivity.this, UserActivity.class));
                     return;
-                }else if((spPsw!=null&&!TextUtils.isEmpty(spPsw)&&!md5Psw.equals(spPsw))){
+                }else if((pas!=null||!uerName.equals(uer)||!password.equals(pas))){
                     Toast.makeText(LoginActivity.this, "输入的用户名和密码不一致", Toast.LENGTH_SHORT).show();
                     return;
                 }else{
@@ -76,48 +63,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
-    /**
-     *从SharedPreferences中根据用户名读取密码
-     */
-    private String readPsw(String userName){
-        //getSharedPreferences("loginInfo",MODE_PRIVATE);
-        //"loginInfo",mode_private; MODE_PRIVATE表示可以继续写入
-        SharedPreferences sp=getSharedPreferences("loginInfo", MODE_PRIVATE);
-        //sp.getString() userName, "";
-        return sp.getString(userName , "");
-    }
-    /**
-     *保存登录状态和登录用户名到SharedPreferences中
-     */
-    private void saveLoginStatus(boolean status,String userName){
-        //saveLoginStatus(true, userName);
-        //loginInfo表示文件名  SharedPreferences sp=getSharedPreferences("loginInfo", MODE_PRIVATE);
-        SharedPreferences sp=getSharedPreferences("loginInfo", MODE_PRIVATE);
-        //获取编辑器
-        SharedPreferences.Editor editor=sp.edit();
-        //存入boolean类型的登录状态
-        editor.putBoolean("isLogin", status);
-        //存入登录状态时的用户名
-        editor.putString("loginUserName", userName);
-        //提交修改
-        editor.commit();
-    }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        //super.onActivityResult(requestCode, resultCode, data);
-        super.onActivityResult(requestCode, resultCode, data);
-        if(data!=null){
-            //是获取注册界面回传过来的用户名
-            // getExtra().getString("***");
-            String userName=data.getStringExtra("userName");
-            if(!TextUtils.isEmpty(userName)){
-                //设置用户名到 et_user_name 控件
-                user_input.setText(userName);
-                //et_user_name控件的setSelection()方法来设置光标位置
-                user_input.setSelection(userName.length());
-            }
-        }
-    }
 }
 
    // @Override
--- app/src/main/java/com/example/date/RegisterActivity.java
+++ app/src/main/java/com/example/date/RegisterActivity.java
@@ -15,58 +15,70 @@ import android.widget.Toast;
 public class RegisterActivity extends AppCompatActivity {
     private Button btn_register;//注册按钮
     //用户名，密码，再次输入的密码的控件
-    private EditText user,password,password1;
+    private EditText user, password, password1;
     //用户名，密码，再次输入的密码的控件的获取值
-    private String userName,psw,pswAgain;
+    private String userName, psw, pswAgain, uer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         //设置页面布局 ,注册界面
-        setContentView(R.layout.activity_register);
-        //设置此界面为竖屏
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+//        setContentView(R.layout.activity_register);
+//        //设置此界面为竖屏
+//        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
         init();
     }
 
     private void init() {
 
-        btn_register=findViewById(R.id.btn_register);
-        user=findViewById(R.id.user);
-        password=findViewById(R.id.password);
-        password1=findViewById(R.id.password1);
+        btn_register = findViewById(R.id.btn_register);
+        user = findViewById(R.id.user);
+        password = findViewById(R.id.password);
+        password1 = findViewById(R.id.password1);
         //注册按钮
-        btn_register.setOnClickListener(new View.OnClickListener(){
+        btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //获取输入在相应控件中的字符串
-                getEditString();
+                userName = user.getText().toString().trim();
+                psw = password.getText().toString().trim();
+                pswAgain = password1.getText().toString().trim();
+                SharedPreferences sp = getSharedPreferences("loginInfo", MODE_PRIVATE);
+                uer = sp.getString("userName", "");
+
+
                 //判断输入框内容
-                if(TextUtils.isEmpty(userName)){
+                if (userName.length() <= 0) {
                     Toast.makeText(RegisterActivity.this, "请输入用户名", Toast.LENGTH_SHORT).show();
                     return;
-                }else if(TextUtils.isEmpty(psw)){
+                } else if (psw.length() <= 0) {
                     Toast.makeText(RegisterActivity.this, "请输入密码", Toast.LENGTH_SHORT).show();
                     return;
-                }else if(TextUtils.isEmpty(pswAgain)){
+                } else if (pswAgain.length() <= 0) {
                     Toast.makeText(RegisterActivity.this, "请再次输入密码", Toast.LENGTH_SHORT).show();
                     return;
-                }else if(!psw.equals(pswAgain)){
+                } else if (!psw.equals(pswAgain)) {
                     Toast.makeText(RegisterActivity.this, "输入两次的密码不一样", Toast.LENGTH_SHORT).show();
                     return;
                     /**
                      *从SharedPreferences中读取输入的用户名，判断SharedPreferences中是否有此用户名
                      */
-                }else if(isExistUserName(userName)){
+                } else if (userName.equals(uer)) {
                     Toast.makeText(RegisterActivity.this, "此账户名已经存在", Toast.LENGTH_SHORT).show();
                     return;
-                }else{
+                } else {
                     Toast.makeText(RegisterActivity.this, "注册成功", Toast.LENGTH_SHORT).show();
                     //把账号、密码和账号标识保存到sp里面
                     /**
                      * 保存账号和密码到SharedPreferences中
                      */
-                    saveRegisterInfo(userName, psw);
+                    SharedPreferences sp2 = getSharedPreferences("loginInfo", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sp2.edit();
+                    editor.putString("userName", userName);
+                    editor.putString("psw", psw);
+                    editor.commit();
+//                    saveRegisterInfo(userName, psw);
                     //注册成功后把账号传递到LoginActivity.java中
                     // 返回值到loginActivity显示
                     Intent data = new Intent();
@@ -79,49 +91,4 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
     }
-    /**
-     * 获取控件中的字符串
-     */
-    private void getEditString(){
-        userName=user.getText().toString().trim();
-        psw=password.getText().toString().trim();
-        pswAgain=password1.getText().toString().trim();
-    }
-    /**
-     * 从SharedPreferences中读取输入的用户名，判断SharedPreferences中是否有此用户名
-     */
-    private boolean isExistUserName(String userName){
-        boolean has_userName=false;
-        //mode_private SharedPreferences sp = getSharedPreferences( );
-        // "loginInfo", MODE_PRIVATE
-        SharedPreferences sp=getSharedPreferences("loginInfo", MODE_PRIVATE);
-        //获取密码
-        String spPsw=sp.getString(userName, "");//传入用户名获取密码
-        //如果密码不为空则确实保存过这个用户名
-        if(!TextUtils.isEmpty(spPsw)) {
-            has_userName=true;
-        }
-        return has_userName;
-    }
-    /**
-     * 保存账号和密码到SharedPreferences中SharedPreferences
-     */
-    private void saveRegisterInfo(String userName,String psw){
-      //  String md5Psw = MD5Utils.md5(psw);//把密码用MD5加密
-        //loginInfo表示文件名, mode_private SharedPreferences sp = getSharedPreferences( );
-        SharedPreferences sp=getSharedPreferences("loginInfo", MODE_PRIVATE);
-        //获取编辑器， SharedPreferences.Editor  editor -> sp.edit();
-        SharedPreferences.Editor editor=sp.edit();
-        //以用户名为key，密码为value保存在SharedPreferences中
-        //key,value,如键值对，editor.putString(用户名，密码）;
-        editor.putString(userName, psw);
-        //提交修改 editor.commit();
-        editor.commit();
-    }
 }
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_register);
-//    }
